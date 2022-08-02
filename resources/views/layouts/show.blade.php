1881b4fe010a7263f9d922fe2605d9be163fe10a
Vozniuk Kateryna <newstory77777@gmail.com>
2022-08-02 18:33:37 +0300

    Chart add

--- resources/views/layouts/show.blade.php
+++ resources/views/layouts/show.blade.php
@@ -4,12 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.js" charset="utf-8"></script>
+
     <title>Show Page</title>
 </head>
 <body>
 <div class="container">
     @yield('content')
 </div>
+
 </body>
 </html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -24,4 +24,55 @@
         @endforeach
         </tbody>
     </table>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading"><b>Charts</b></div>
+                <div class="panel-body">
+                    <canvas id="canvas" height="280" width="600"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        var Dates = new Array();
+        var Opens = new Array();
+        var Closes = new Array();
+        $(document).ready(function () {
+            var data = @json($prices);
+            data.forEach(function (d) {
+                Dates.push(d.format_date);
+                Closes.push(d.close);
+                Opens.push(d.open);
+            });
+            var ctx = document.getElementById("canvas").getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: Dates,
+                    datasets: [{
+                        label: 'Open price',
+                        data: Opens,
+                        borderColor: "#de4f54"
+                    }, {
+                        label: 'Close price',
+                        data: Closes,
+                        borderColor: "blue"
+
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+        });
+    </script>
+
 @endsection
